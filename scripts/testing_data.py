0c57a4b0f0f32a8cc3ac1301a0a95e8d2c191347
Mehdi Samouh <mehdi.samouh1998@gmail.com>
2026-04-03 18:17:35 +0200

    Add dataset download functionality to testing_data.py
    Replaced print statement with dataset download functionality using kagglehub.

--- scripts/testing_data.py
+++ scripts/testing_data.py
@@ -1,1 +1,6 @@
-print("hello")
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("alexjr2001/formula-1-dataset-race-data-and-telemetry")
+
+print("Path to dataset files:", path)
